83181218ea59c84dc14c2bed11b42cb67a76d748
bswck <bartosz@slawecki.dev>
2025-10-23 14:18:05 +0200

    Finalize (almost)

--- dedupe.py
+++ dedupe.py
@@ -6,42 +6,20 @@ from collections.abc import Generator
 from contextlib import contextmanager
 from itertools import product
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, NamedTuple
 from unittest.mock import patch
 
 if TYPE_CHECKING:
     from _typeshed import StrPath
 
-platforms = ["linux", "darwin", "win32"]
-versions = [(3, 10), (3, 11), (3, 12), (3, 13), (3, 14)]
+# this is basically useless until we examine all platforms
+all_platforms = ["linux", "darwin", "win32"]
+all_versions = [(3, 10), (3, 11), (3, 12), (3, 13), (3, 14)]
 
 current_indent = 0
 indent_width = 4
-platform_guard_tpl = "if sys.platform == {platform}:"
-version_guard_tpl = "if sys.version_info[:2] == {version}:"
-annotation_tpl = "{name}: Final[Literal[{value}]]"
-
-type Errnos = dict[str | None, dict[tuple[int, int], dict[str, str]]]
-
-
-def dedupe_from_script(script_path: StrPath) -> Errnos:
-    # {platform: {version: {name: value}}}
-    by_platform = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
-    # {name: {value: {platform: [version, ...]}}}
-    by_name = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-    script = Path(script_path).read_text()
-
-    for platform, version in product(platforms, versions):
-        prev_version = versions[max(0, versions.index(version) - 1)]
-
-        with patch("sys.platform", platform), patch("sys.version_info", version):
-            errnos_for_env = {}
-            exec(script, {"sys": sys}, errnos_for_env)
-
-            for name, value in sorted(errnos_for_env.items()):
-                by_name[name][value][platform].append(version)
-    exit("wip")
-    return by_platform
+literal_annotation_tpl = "{name}: Final[Literal[{value}]]"
+relaxed_annotation_tpl = "{name}: Final[int]"  # unused
 
 
 def emit(s: str) -> None:
@@ -49,33 +27,117 @@ def emit(s: str) -> None:
 
 
 @contextmanager
-def indentation(only_if: bool = True) -> Generator[None]:
+def indent(only_if: bool = True) -> Generator[None]:
     global current_indent
     current_indent += indent_width * only_if
     yield
     current_indent -= indent_width * only_if
 
 
-def produce_deduped_stub(errnos: Errnos) -> None:
-    emit("import sys")
-    emit("from typing import Final, Literal")
+class VersionRange(NamedTuple):
+    versions: list[tuple[int, int]]
 
-    for platform, versions in errnos.items():
-        emit(platform_guard_tpl.format(platform=platform))
+    def to_expression(self) -> str:
+        atom = "sys.version_info()[:2]"
+        match self.versions:
+            case [single]:
+                if single == all_versions[-1]:
+                    return f"{atom} >= {single}"
+                if single == all_versions[0]:
+                    return ""
+                return f"{atom} == {single}"
+            case full if full == all_versions:
+                return ""
+            case [first, *rest, last]:
+                f_maj, f_min = first
+                l_maj, l_min = last
+                assert f_maj == l_maj
+                assert f_min + 1 + len(rest) == l_min
+                assert last == all_versions[-1]
+                return f"{atom} >= {first}"
+            case r:
+                raise AssertionError(f"invalid version range: {r}")
 
-        with indentation():
-            for version, names in versions.items():
-                with indentation(only_if=version is not None):
-                    if version:
-                        emit(version_guard_tpl.format(version=version))
 
-                    for name, value in names.items():
-                        with indentation():
-                            emit(annotation_tpl.format(name=name, value=value))
+class PlatformRange(NamedTuple):
+    platforms: list[str]
+
+    def to_expression(self) -> str:
+        atom = "sys.platform"
+        match self.platforms:
+            case full if full == all_platforms:
+                return ""
+            case [*targeted_platforms] if len(targeted_platforms) >= 1:
+                # question: would type checkers understand membership test?
+                # note: we'd need to create "generalizations" with windows specialcased... unless we check all systems
+                return " or ".join(
+                    [
+                        f"{atom} == {platform.join('""')}"
+                        for platform in targeted_platforms
+                    ]
+                )
+            case _:
+                raise AssertionError("no platforms")
+
+
+def dedupe_from_script(script_path: StrPath) -> None:
+    # {name: {value: {platform: [version, ...]}}}
+    by_name: dict[str, dict[int, dict[str, list[tuple[int, int]]]]] = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    script = Path(script_path).read_text()
+
+    for platform, version in product(all_platforms, all_versions):
+        with patch("sys.platform", platform), patch("sys.version_info", version):
+            errnos_for_env: dict[str, int] = {}
+            exec(script, errnos_for_env)
+
+            for name, value in sorted(errnos_for_env.items()):
+                if name.isupper():
+                    by_name[name][value][platform].append(version)
+
+    if by_name:
+        emit("import sys")
+        emit("from typing import Final, Literal")
+
+    conditional_blocks: dict[str, list[str]] = defaultdict(list)
+
+    for name, values in by_name.items():
+        for value, platforms in values.items():
+            platform_range = PlatformRange(list(platforms))
+            platform_clause = platform_range.to_expression()
+            version_clauses = set()
+            for platform, versions in platforms.items():
+                version_range = VersionRange(versions)
+                version_clause = version_range.to_expression()
+                version_clauses.add(version_clause)
+            if len(version_clauses) > 1:
+                raise AssertionError(
+                    "same value of same errno was introduced in different python version"
+                )
+            relevant_clauses = list(filter(bool, (platform_clause, version_clause)))
+            clause = " and ".join(
+                [
+                    subclause.join(
+                        "()"
+                        if " or " in subclause and len(relevant_clauses) > 1
+                        else ("", "")
+                    )
+                    for subclause in relevant_clauses
+                ]
+            )
+            conditional_blocks[clause].append(
+                literal_annotation_tpl.format(name=name, value=value)
+            )
+
+    for condition, statements in conditional_blocks.items():
+        if condition:
+            emit(f"if {condition}:")
+        with indent(only_if=bool(condition)):
+            for statement in statements:
+                emit(statement)
 
 
 def main(script_path: StrPath) -> None:
-    produce_deduped_stub(dedupe_from_script(script_path))
+    dedupe_from_script(script_path)
 
 
 if __name__ == "__main__":
