90ce9d012230d90aa7f37c66ed3af7d31625c7d7
Bartosz Sławecki <bartosz@slawecki.dev>
2025-10-23 05:18:30 +0200

    Try a different hack

--- stuff.py
+++ stuff.py
@@ -8,13 +8,13 @@ code = urllib.request.urlopen(
     "https://raw.githubusercontent.com/python/typeshed/c7f70d8ac9310823a2fe5bbe0d276bac9eb314d3/stdlib/errno.pyi"
 ).read().decode()
 
-ns = {}
-exec(code, ns)
-relevant_to_this_os = ns["__annotations__"]
+os_relevant = {}
+exec(code.replace("Final[int]", "Final[int] = ..."), ns)
 
 for name, value in vars(errno).items():
-    if name in relevant_to_this_os:
+    if name in os_relevant:
         code = re.sub(pat.format(name=name), rf"\g<1>[Literal[{value}]]", code)
 
 print(code)
+
 
