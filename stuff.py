97413b80462079c956ccb97aa8865a4837a51a5f
Bartosz Sławecki <bartosz@slawecki.dev>
2025-10-23 05:14:42 +0200

    Only change relevant OS annotations

--- stuff.py
+++ stuff.py
@@ -8,7 +8,13 @@ code = urllib.request.urlopen(
     "https://raw.githubusercontent.com/python/typeshed/c7f70d8ac9310823a2fe5bbe0d276bac9eb314d3/stdlib/errno.pyi"
 ).read().decode()
 
+ns = {}
+exec(code, ns)
+relevant_to_this_os = ns["__annotations__"]
+
 for name, value in vars(errno).items():
-    code = re.sub(pat.format(name=name), rf"\g<1>[Literal[{value}]]", code)
+    if name in relevant_to_this_os:
+        code = re.sub(pat.format(name=name), rf"\g<1>[Literal[{value}]]", code)
 
 print(code)
+
