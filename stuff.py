bde224c01e1228eae79aaf6e0016ae903dd4ddf9
Bartosz Sławecki <bartosz@slawecki.dev>
2025-10-23 04:43:11 +0200

    Errno literals let's go

--- stuff.py
+++ stuff.py
@@ -1,4 +1,14 @@
-import os
+import errno
+import re
+import urllib.request
 
-prog = r"C:\Windows\System32\whoami.exe"
-os.execve(prog, [prog], os.environ)
+pat = r"({name}: Final)\[(int)\]"
+
+code = urllib.request.urlopen(
+    "https://raw.githubusercontent.com/python/typeshed/c7f70d8ac9310823a2fe5bbe0d276bac9eb314d3/stdlib/errno.pyi"
+).read().decode()
+
+for name, value in vars(errno).items():
+    code = re.sub(pat.format(name=name), rf"\g<1>[Literal[{value}]]", code)
+
+print(code)
