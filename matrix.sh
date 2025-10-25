9e5d8e40ccb7fa99d162b9eb4170a45e8a8cb84d
bswck <bartosz@ilikepython.com>
2025-10-25 03:00:32 +0200

    Don't run on alphas/betas/rcs

--- matrix.sh
+++ matrix.sh
@@ -4,5 +4,5 @@ set -HCETeuxo pipefail
 : && \
     curl -fsSL 'https://raw.githubusercontent.com/actions/python-versions/main/versions-manifest.json' \
     | jq '.[].version' \
-    | sed -E -e 's/"(.*)"/- \1/' -e '/3\.[0-8]\./d'
+    | sed -E -e 's/"(.*)"/- \1/' -e '/3\.[0-8]\./d' -e '/(-rc|beta|alpha)/d'
 
